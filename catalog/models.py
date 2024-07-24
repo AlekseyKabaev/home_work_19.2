a36e4e1cbe9169059248fae440c27759f6ba4fd2
Alexey Kabaev <kabaev.alexey@mail.ru>
2024-07-25 00:13:49 +0300

    fixed models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название категории')
+    description = models.TextField(verbose_name='Описание категории', null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название продукта')
+    description = models.TextField(verbose_name='Описание продукта', null=True, blank=True)
+    image = models.ImageField(upload_to="catalog/image", verbose_name='Изображение', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', null=True, blank=True)
+    price = models.IntegerField(verbose_name='Стоимость продукта')
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
