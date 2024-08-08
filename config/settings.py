76f9bf56dadb1d3144d372f1f229a03ac1f60d8e
Alexey Kabaev <kabaev.alexey@mail.ru>
2024-08-08 21:33:38 +0300

    changed settings.py

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres1',
         'USER': 'postgres',
-        'PASSWORD': ""
+        'PASSWORD': "905Shady19"
     }
 }
 
