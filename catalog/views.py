b0d932f04f2ae90d6b9ef197aafb9f60802e3a78
Alexey Kabaev <kabaev.alexey@mail.ru>
2024-08-13 23:58:34 +0300

    change views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
+from django.views.generic import ListView, DetailView, TemplateView
 
 
-def products_list(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'products_list.html', context)
+class ProductListView(ListView):
+    model = Product
 
 
-def products_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'products_detail.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class ContactPageView(TemplateView):
+    template_name = "contacts.html"
+
+    def post(self, request):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            print(f'You have new message from {name}({phone}): {message}')
+        return render(request, "contacts.html")
+
+# def products_list(request):
+#     products = Product.objects.all()
+#     context = {'products': products}
+#     return render(request, 'products_list.html', context)
+
+
+# def products_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {'product': product}
+#     return render(request, 'product_detail.html', context)
 
 # def home(request):
 #     latest_products = Product.objects.order_by('-updated_at')[:5]
@@ -20,10 +40,4 @@ def products_detail(request, pk):
 #     return render(request, "home.html", {'latest_products': latest_products})
 #
 #
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'You have new message from {name}({phone}): {message}')
-#     return render(request, "contacts.html")
+
