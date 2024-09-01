3fa90b323cf4bba7faa6fd0addc33f4dda228f2c
AlekseyKabaev <165834508+AlekseyKabaev@users.noreply.github.com>
2024-09-01 19:02:19 +0300

    change config files

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
-from catalog.models import Product, Version
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
-from catalog.forms import ProductForm, VersionForm
 from django.forms import inlineformset_factory
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from catalog.models import Product, Version
+from catalog.forms import ProductForm, VersionForm
 
 
 class ProductListView(ListView):
@@ -24,10 +26,17 @@ class ProductDetailView(DetailView):
     model = Product
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(CreateView, LoginRequiredMixin):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:products_list')
+
+    def form_valid(self, form):
+        product = form.save()
+        user = self.request.user
+        product.owner = user
+        product.save()
+        return super().form_valid(form)
 
 
 class ProductUpdateView(UpdateView):
